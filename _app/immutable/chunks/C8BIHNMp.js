1abac296b07c8d883f3e6a2fb89adc157cff21f8
maxhoare61 <maxhoare61@gmail.com>
2025-01-24 19:46:25 +1100

    Updates

---
+++ _app/immutable/chunks/C8BIHNMp.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1c0ikd4)==null?void 0:s.base)??"/downstream-project";var e;const a=((e=globalThis.__sveltekit_1c0ikd4)==null?void 0:e.assets)??t;export{a,t as b};
